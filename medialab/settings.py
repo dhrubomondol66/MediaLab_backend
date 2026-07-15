79a368b280aae48c2dbaf83214ebba9f056d2f42
dhrubomondol66 <dhrubomondol66@gmail.com>
2026-07-15 17:25:42 +0600

    minor changes

--- medialab/settings.py
+++ medialab/settings.py
@@ -25,12 +25,12 @@ environ.Env.read_env(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-viehn)ph99qy)ondfacaw=&bz_-2=nas$y=u)o#4e_o^a@%ll%"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["medialab-backend-zbrj.onrender.com", "localhost", "127.0.0.1"])
 
 
 # Application definition
